2ddd11f48b337e2565c796b3e039593d9b411ecc
庞云龙 <pangyunlong02@meituan.com>
2018-07-14 11:08:22 +0800

    gitignore init

---
+++ src/main/java/com/pracrice/git/specimen/SpecimenInfo.java
@@ -0,0 +1,19 @@
+package com.pracrice.git.specimen;
+
+/**
+ * 文件样本信息
+ * @author pangyunlong
+ * @date 2018/7/14
+ */
+public class SpecimenInfo {
+
+    private String phases = "init git";
+
+
+    @Override
+    public String toString() {
+        return "SpecimenInfo{" +
+                "phases='" + phases + '\'' +
+                '}';
+    }
+}
